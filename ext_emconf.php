c75bb8900fd0b44b6bf0b139b9b57407273c578d
Boris Schauer <me@bschauer.de>
2016-05-11 14:46:55 +0200

    [TASK] Flexform downgrade

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,19 +4,12 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Slick Carousel',
     'description' => 'Slick Carousel (Slider) - Responsive, Multiple Items, Center Mode, Lazy Loading, Autoplay, Slider Syncing (Gallery)',
     'category' => 'templates',
-    'version' => '0.3.1',
+    'version' => '0.0.62',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Boris Schauer, Oliver Oertel',
     'author_email' => 'me@bschauer.de, oertel@analog.de',
     'author_company' => 'bschauer, analog',
     'constraints' => array(
-        'depends' => array(
-            'typo3' => '7.6.0-7.99.99',
-            'vhs' => '2.3.3-2.4.99',
-        ),
-        'conflicts' => array(
-
-        ),
     ),
 );
